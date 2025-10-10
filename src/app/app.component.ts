4bde5e17c01bf6dbce9bea2f293adab1696c328e
Pratik Dutta <pratikdutta@Pratiks-MacBook-Air.local>
2025-10-10 19:53:49 +0530

    the set up done and also the routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from '../views/header/app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
