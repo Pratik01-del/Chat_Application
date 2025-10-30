16a448f052a182cc63c13461daea91224bc64372
Pratik Dutta <pratikdutta@Pratiks-MacBook-Air.local>
2025-10-30 19:31:29 +0530

    the landing page is not complete yet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from '../views/main/dashboard/dashboard.component';
+import { LoginPageComponent } from '../views/login-page/login-page.component';
 
 export const routes: Routes = [
 
     {
-        path: '', component: DashboardComponent
+        path: '', component: LoginPageComponent
+    },
+
+    {
+        path: 'dashboard', component: DashboardComponent
     }
 ];
